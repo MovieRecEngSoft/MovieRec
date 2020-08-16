5e4501f415ab0f7e4e697ef0d524367abf3f4885
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-15 21:10:07 -0300

    App.js initialization

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import RegisterForm from './Pages/RegisterForm';
+
+const App = () => (
+  <div className="App">
+    <RegisterForm />
+  </div>
+);
+
+export default App;
