784ed4d4e4d18b75aff40ab0393a7a1ee5df1203
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-18 20:52:53 -0300

    added form to login page

--- web/src/Pages/Login/index.js
+++ web/src/Pages/Login/index.js
@@ -1,81 +1,18 @@
-import React from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
+import React from "react";
+import LoginForm from "../../Components/LoginForm";
 
 import './styles.css';
 
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 16,
-  },
-};
-
-const Login = () => {
-  const onFinish = values => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
-
+function Login() {
   return (
-    <div className="content">
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your username!',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+    <>
+      <div className="bg-image">
+        <div className="page-register">
+          <LoginForm title="Login" />
+        </div>
+      </div>
+    </>
   );
-};
+}
 
 export default Login;
