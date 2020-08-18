294c114d96f1dcf59d2cee66407d74ea9b513410
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-18 20:22:04 -0300

    added signup form with antd

---
+++ web/src/Components/SignupForm/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Form } from "antd";
+
+import "./styles.css";
+import Input from "../Input";
+import Button from "../Button";
+
+const SignupForm = ({title}) => {
+    const layout = {
+          labelCol: {
+          span: 8,
+        },
+          wrapperCol: {
+          span: 16,
+        },
+    };
+
+    const validateMessages = {
+        required: "${label} is required!",
+        types: {
+            email: "${label} is not validate email!",
+            number: "${label} is not a validate number!",
+        },
+        number: {
+            range: "${label} must be between ${min} and ${max}",
+        },
+    };
+
+    const onFinish = (values) => {
+        console.log(values);
+    };
+
+    return (
+      <>
+      <div className="content">
+        <Form
+          {...layout}
+          name="nest-messages"
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+          >
+          <h1>{title}</h1>
+          <Input name="name" placeholder="Name" />
+
+          <Input name="email" placeholder="Email" />
+
+          <Input name="password" type="password" placeholder="Password" />
+
+          <Input name="c-password" type="password" placeholder="Confirm Password" />
+
+          <Button type="button" htmlType="submit" name="Register" />
+        </Form>
+      </div>
+    </>
+    );
+};
+
+export default SignupForm;
