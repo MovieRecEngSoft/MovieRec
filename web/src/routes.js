1f7436570dbb68f49631cf5681bdf49cdb300af7
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-18 20:25:15 -0300

    updated route for signup page

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './Pages/Login';
-import RegisterForm from './Pages/RegisterForm';
 import Home from './Pages/Home';
+import Register from './Pages/Register';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Home} />
             <Route path="/login" exact component={Login} />
-            <Route path="/signup" component={RegisterForm} />
+            <Route path="/signup" component={Register} />
         </BrowserRouter>
     );
 }
