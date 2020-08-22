7a565e547fdc6bf9cea6cc332df79f3dabdc961a
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-22 18:58:48 -0300

    added main

--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -4,21 +4,28 @@ import Carousel from '../../Components/Carousel';
 import Menu from '../../Components/Menu';
 
 import dadosIniciais from "../../data/dados_iniciais.json";
+import Main from '../../Components/Main';
 
 function Home(){
     return (
       <>
         <Menu />
-        <div className="bg-image">
-          <div className="">
-            
-            <br />
-            <Carousel category={dadosIniciais.categorias[0]} />
-            <br /><br />
-            <Carousel category={dadosIniciais.categorias[1]} />
-          
-          </div>
-        </div>
+        <Main background="https://nit.pt/wp-content/uploads/2019/12/76674270a70d88a6fbfb0428f55d10cb-754x394.jpg">
+          <br />
+          <br />
+          <br />
+          <br />
+          <Carousel category={dadosIniciais.categorias[0]} />
+
+          <br />
+          <Carousel category={dadosIniciais.categorias[2]} />
+
+          <br />
+          <Carousel category={dadosIniciais.categorias[3]} />
+
+          <br />
+          <Carousel category={dadosIniciais.categorias[4]} />
+        </Main>
       </>
     );
 }
