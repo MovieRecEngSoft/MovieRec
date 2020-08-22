12ffa54c66cb2faed3c955b9715727b2ac80d577
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-22 18:58:32 -0300

    added main

--- web/src/Pages/Register/index.js
+++ web/src/Pages/Register/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import SignupForm from "../../Components/SignupForm";
 
 import './styles.css';
+import Main from "../../Components/Main";
+import Menu from "../../Components/Menu";
 
 function Register() {
   return (
     <>
-      <div className="bg-image">
+      <Menu />
+      <Main background="https://i.pinimg.com/564x/4c/86/60/4c86600a82bb109d4e9ae48c993d73a0.jpg">
         <div className="page-register">
           <SignupForm title="Sign Up" />
         </div>
-      </div>
+      </Main>
     </>
   );
 }
