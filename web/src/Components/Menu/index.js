af9febd17d7a1c9028d0ec817831ac00980d654c
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-22 17:06:42 -0300

    created menu bar

---
+++ web/src/Components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import { UserOutlined } from "@ant-design/icons";
+
+
+import './styles.css';
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="Logo" />
+      </Link>
+      <Link to="/profile">
+        <UserOutlined className="user-icon" />
+      </Link>
+    </nav>
+  );
+}
+
+export default Menu;
