8b2e687258bb6fea4f10145a12aa6852b1864d1c
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-22 18:58:41 -0300

    added main

--- web/src/Pages/Login/index.js
+++ web/src/Pages/Login/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import LoginForm from "../../Components/LoginForm";
 
 import './styles.css';
+import Main from "../../Components/Main";
+import Menu from "../../Components/Menu";
 
 function Login() {
   return (
     <>
-      <div className="bg-image">
+      <Menu />
+      <Main background="https://i.pinimg.com/564x/4c/86/60/4c86600a82bb109d4e9ae48c993d73a0.jpg">
         <div className="page-register">
           <LoginForm title="Login" />
         </div>
-      </div>
+      </Main>
     </>
   );
 }
