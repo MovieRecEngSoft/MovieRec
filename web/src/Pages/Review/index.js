32078fe3848c03137227e79c0793625b467a5630
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-26 00:20:35 -0300

    Created review page

---
+++ web/src/Pages/Review/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+import './styles.css';
+import Main from "../../Components/Main";
+import Menu from "../../Components/Menu";
+import Review from "../../Components/Review";
+
+function ReviewPage(url) {
+  return (
+    <>
+      <Menu />
+      <Main background="https://www.xmple.com/wallpaper/highlight-black-red-gradient-linear-2736x1824-c2-000000-8b0000-l-67-a-345-f-21.svg">
+        <Review
+          url="https://www.youtube.com/watch?v=ajKuNfOuAkQ"
+          title="The Devil Wears Prada"
+          text="O Labirinto do Fauno (El Laberinto del Fauno, 2006) é com certeza um
+          dos melhores filmes do ano. O cineasta Guillermo Del Toro apresenta
+          uma fábula sombria recheada de metáforas e alegorias. Além de ser puro
+          entretenimento, o longa também é uma ótima refeição mental para os
+          cinéfilos e amantes da literatura fantástica. É fácil encontrar
+          referências a filmes como O Iluminado, A Lenda do Cavaleiro Sem
+          Cabeça, O Mágico de Oz, Hellboy (do próprio Del Toro) e livros como
+          Alice no País das Maravilhas e as fábulas de Hans Christian Andersen e
+          dos Irmãos Grimm.
+          O Labirinto do Fauno (El Laberinto del Fauno, 2006) é com certeza um
+          dos melhores filmes do ano. O cineasta Guillermo Del Toro apresenta
+          uma fábula sombria recheada de metáforas e alegorias. Além de ser puro
+          entretenimento, o longa também é uma ótima refeição mental para os
+          cinéfilos e amantes da literatura fantástica. É fácil encontrar
+          referências a filmes como O Iluminado, A Lenda do Cavaleiro Sem
+          Cabeça, O Mágico de Oz, Hellboy (do próprio Del Toro) e livros como
+          Alice no País das Maravilhas e as fábulas de Hans Christian Andersen e
+          dos Irmãos Grimm.
+          O Labirinto do Fauno (El Laberinto del Fauno, 2006) é com certeza um
+          dos melhores filmes do ano. O cineasta Guillermo Del Toro apresenta
+          uma fábula sombria recheada de metáforas e alegorias. Além de ser puro
+          entretenimento, o longa também é uma ótima refeição mental para os
+          cinéfilos e amantes da literatura fantástica. É fácil encontrar
+          referências a filmes como O Iluminado, A Lenda do Cavaleiro Sem
+          Cabeça, O Mágico de Oz, Hellboy (do próprio Del Toro) e livros como
+          Alice no País das Maravilhas e as fábulas de Hans Christian Andersen e
+          dos Irmãos Grimm.
+          O Labirinto do Fauno (El Laberinto del Fauno, 2006) é com certeza um
+          dos melhores filmes do ano. O cineasta Guillermo Del Toro apresenta
+          uma fábula sombria recheada de metáforas e alegorias. Além de ser puro
+          entretenimento, o longa também é uma ótima refeição mental para os
+          cinéfilos e amantes da literatura fantástica. É fácil encontrar
+          referências a filmes como O Iluminado, A Lenda do Cavaleiro Sem
+          Cabeça, O Mágico de Oz, Hellboy (do próprio Del Toro) e livros como
+          Alice no País das Maravilhas e as fábulas de Hans Christian Andersen e
+          dos Irmãos Grimm.
+          O Labirinto do Fauno (El Laberinto del Fauno, 2006) é com certeza um
+          dos melhores filmes do ano. O cineasta Guillermo Del Toro apresenta
+          uma fábula sombria recheada de metáforas e alegorias. Além de ser puro
+          entretenimento, o longa também é uma ótima refeição mental para os
+          cinéfilos e amantes da literatura fantástica. É fácil encontrar
+          referências a filmes como O Iluminado, A Lenda do Cavaleiro Sem
+          Cabeça, O Mágico de Oz, Hellboy (do próprio Del Toro) e livros como
+          Alice no País das Maravilhas e as fábulas de Hans Christian Andersen e
+          dos Irmãos Grimm.
+          O Labirinto do Fauno (El Laberinto del Fauno, 2006) é com certeza um
+          dos melhores filmes do ano. O cineasta Guillermo Del Toro apresenta
+          uma fábula sombria recheada de metáforas e alegorias. Além de ser puro
+          entretenimento, o longa também é uma ótima refeição mental para os
+          cinéfilos e amantes da literatura fantástica. É fácil encontrar
+          referências a filmes como O Iluminado, A Lenda do Cavaleiro Sem
+          Cabeça, O Mágico de Oz, Hellboy (do próprio Del Toro) e livros como
+          Alice no País das Maravilhas e as fábulas de Hans Christian Andersen e
+          dos Irmãos Grimm."
+        />
+      </Main>
+    </>
+  );
+}
+
+export default ReviewPage;
