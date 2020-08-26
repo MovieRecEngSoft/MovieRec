0565d78481af9e7a581eb898168d01c159d89eeb
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-26 00:22:01 -0300

    added route for review page

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,6 +4,7 @@ import Login from './Pages/Login';
 import Home from './Pages/Home';
 import Register from './Pages/Register';
 import Profile from './Pages/Profile';
+import ReviewPage from './Pages/Review';
 
 
 function Routes() {
@@ -13,6 +14,7 @@ function Routes() {
             <Route path="/login" exact component={Login} />
             <Route path="/signup" component={Register} />
             <Route path="/profile" component={Profile} />
+            <Route path="/movie" component={ReviewPage} />
         </BrowserRouter>
     );
 }
