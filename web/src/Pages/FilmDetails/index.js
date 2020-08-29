8c1e19afffc6110971ce12e20d7fa59e92f5cb7d
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-29 18:49:03 -0300

    added component Card

--- web/src/Pages/FilmDetails/index.js
+++ web/src/Pages/FilmDetails/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-import './styles.css';
 import Main from "../../Components/Main";
 import Menu from "../../Components/Menu";
 import getImageAddress from "../../assets/utils/getImageAddress";
+import Card from "../../Components/Card";
+
+import './styles.css';
+import Review from "../../Components/Review";
 
 function FilmDetails(url) {
   const image = getImageAddress("/HORpg5CSkmeQlAolx3bKMrKgfi.jpg");
@@ -15,17 +18,17 @@ function FilmDetails(url) {
         <div className="container-wrapper">
           <div className="row">
             <div className="leftcolumn">
-              <div className="card">
+              <Card>
                 <img className="cover" src={image} />
-              </div>
+              </Card>
             </div>
             <div className="rightcolumn">
-              <div className="card">
+              <Card>
                 <h2 className="film-title">
                   <strong>Skyfall - 007</strong>
                 </h2>
                 <h3 className="film-details">2012 - Directed by Sam Mendes</h3>
-                <p className="review-text">
+                <p className="summary">
                   When Bond’s latest assignment goes gravely wrong and agents
                   around the world are exposed, MI6 is attacked forcing M to
                   relocate the agency. These events cause her authority and
@@ -37,39 +40,51 @@ function FilmDetails(url) {
                   whose lethal and hidden motives have yet to reveal themselves.
                 </p>
                 <br />
-              </div>
-              <div className="card">
-                <h2 className="film-title">TITLE HEADING</h2>
-                <h5>Title description, Dec 7, 2017</h5>
-                <p>
-                  Sunt in culpa qui officia deserunt mollit anim id est laborum
-                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco.
-                </p>
-              </div>
-              <div className="card">
-                <h2 className="film-title">TITLE HEADING</h2>
-                <h5>Title description, Sep 2, 2017</h5>
-                <p>
-                  Sunt in culpa qui officia deserunt mollit anim id est laborum
-                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco.
-                </p>
-              </div>
-              <div className="card">
-                <h2 className="film-title">TITLE HEADING</h2>
-                <h5>Title description, Sep 2, 2017</h5>
-                <div className="fakeimg height200">Image</div>
-                <p>Some text..</p>
-                <p>
-                  Sunt in culpa qui officia deserunt mollit anim id est laborum
-                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco.
-                </p>
-              </div>
+              </Card>
+
+              <br />
+              <h1 className="title-separator">Reviews</h1>
+
+              <Card>
+                <Review
+                  text="Sunt in culpa qui officia deserunt mollit anim id est laborum
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco."
+                  author="Lorena Mendes"
+                  avatar="https://trello-members.s3.amazonaws.com/5ca54b957891488995401310/cd44fb76a31dbd7f303e3feac211c9e3/50.png"
+                />
+              </Card>
+              <Card>
+                <Review
+                  text="Sunt in culpa qui officia deserunt mollit anim id est laborum
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco."
+                  author="Lorena Mendes"
+                  avatar="https://trello-members.s3.amazonaws.com/5ca54b957891488995401310/cd44fb76a31dbd7f303e3feac211c9e3/50.png"
+                />
+              </Card>
+              <Card>
+                <Review
+                  text="Sunt in culpa qui officia deserunt mollit anim id est laborum
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco."
+                  author="Lorena Mendes"
+                  avatar="https://trello-members.s3.amazonaws.com/5ca54b957891488995401310/cd44fb76a31dbd7f303e3feac211c9e3/50.png"
+                />
+              </Card>
+              <Card>
+                <Review
+                  text="Sunt in culpa qui officia deserunt mollit anim id est laborum
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco."
+                  author="Lorena Mendes"
+                  avatar="https://trello-members.s3.amazonaws.com/5ca54b957891488995401310/cd44fb76a31dbd7f303e3feac211c9e3/50.png"
+                />
+              </Card>
               <br />
             </div>
           </div>
