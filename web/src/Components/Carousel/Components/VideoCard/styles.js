5a55a4aa73f7d8d82c35fe9e5db3e6aab01714fe
LorenaMendes <lorenamendes.contato@gmail.com>
2020-08-29 16:37:49 -0300

    width changed

--- web/src/Components/Carousel/Components/VideoCard/styles.js
+++ web/src/Components/Carousel/Components/VideoCard/styles.js
@@ -8,8 +8,8 @@ export const VideoCardContainer = styled.a`
   cursor: pointer;
   color: white;
   flex: 0 0 298px;
-  width: 298px;
-  height: 197px;
+  width: 200px;
+  height: 300px;
   background-image: ${({ url }) => `url(${url})`};
   background-size: cover;
   background-position: center;
