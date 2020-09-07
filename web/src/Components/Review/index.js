534949f92835c2fa41383d5b60b9d88a8da87c67
LorenaMendes <lorenamendes.contato@gmail.com>
2020-09-07 13:03:56 -0300

    likes added

--- web/src/Components/Review/index.js
+++ web/src/Components/Review/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
+import { LikeOutlined, FullscreenOutlined } from "@ant-design/icons";
 import './styles.css';
 
 function Review(props) {
+  let reviewClassName = 'review-text';
+  if(props.type == "review-item"){
+    reviewClassName += ' review-item';
+  }
 
   return (
     <>
-      <div className="row">
-          <img className="avatar" src={props.avatar} />
-          <h5>Review by <strong>{props.author}</strong></h5>
-        </div>
+      <div className="row review-header">
         <div className="row">
-          <p className="review-text">
-            {props.text}
-          </p>
+          <img className="avatar" src={props.avatar} />
+          <h5>
+            Review by <strong>{props.author}</strong>
+          </h5>
         </div>
+        <div className="likes">
+          <p>522</p>
+          <LikeOutlined className="like-button" />
+        </div>
+      </div>
+      
+      <div className="row">
+        <p className={reviewClassName}>{props.text}</p>
+      </div>
     </>
   );
 }
