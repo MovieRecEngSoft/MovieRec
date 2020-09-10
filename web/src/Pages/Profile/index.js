422223dee191dad41158b1cefae434d633a98b22
Igor Henrique <igorhenri5@hotmail.com>
2020-09-10 03:05:10 -0300

    Profile + feed activity update

--- web/src/Pages/Profile/index.js
+++ web/src/Pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import './styles.css';
 import Menu from "../../Components/Menu";
+import Button from "../../Components/Button";
 
 // Gerenciar perfil
 // Como usuário, eu quero ter um ambiente no sistema que represente meu perfil, onde possa apresentar uma imagem como avatar e compartilhar informações sobre mim.
@@ -14,8 +15,16 @@ function Profile() {
         <div class="profile-block">
           <div class="pfsection imgsection">
             <div class="pfimgblock">
-              <img class="avatar" src="https://i.imgur.com/UctWXrz.png" />
+              <img class="avatar" src="https://pbs.twimg.com/profile_images/1295555226720641026/gTnCg1RF_400x400.jpg" />
             </div>
+            <div class="bkground">
+            </div>
+            {/* <div class="bkground">
+            </div> */}
+            <div class="follow-button" >Follow</div>
+            
+            {/* <Button type="button" htmlType="submit" name="Register" class="follow-button" /> */}
+
           </div>
           <div class="pfsection txtsection">
             <div class="txtblk1">
@@ -23,16 +32,91 @@ function Profile() {
             </div>
             <div class="txtblk2">
               <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum et dolor vitae sapien ullamcorper suscipit at vitae
-                odio. Nunc in dignissim enim. Fusce venenatis sagittis ipsum,
-                vitae elementum metus fringilla congue. Curabitur condimentum
-                libero orci, eget imperdiet purus consectetur eget.
+              #Android is made for everyone. Follow along for the latest updates and stories behind our tech. Questions? Get assistance by using #AndroidHelp.
               </span>
             </div>
           </div>
+
           <div class="pfsection timeline">
-            Array [Atividades (resenhas publicadas,comentários ...)]
+            <div class="timeline-node">
+              <div class="avatar-box">
+                <div>
+                  <a>
+                    <img class="avatar-miniature" src="https://i.imgur.com/UctWXrz.png" />
+                  </a>
+                </div>
+              </div>
+              <div class="content-box">
+                <div class="pre-textual">
+                  <span>Rusro</span>
+                  <span class="action">publicou uma nova resenha</span>
+                </div>
+                <div class="text-content">
+                  Even with the challenges of going back to school, Peter and Anthony understand their need for one another, and that together, they can tackle whatever comes their way.
+                </div>
+              </div>
+            </div>
+            <div class="blank-spacing"></div>
+
+            <div class="timeline-node">
+              <div class="avatar-box">
+                <div>
+                  <a>
+                    <img class="avatar-miniature" src="https://i.imgur.com/UctWXrz.png" />
+                  </a>
+                </div>
+              </div>
+              <div class="content-box">
+                <div class="pre-textual">
+                  <span>Rusro</span>
+                  <span class="action">publicou uma nova resenha</span>
+                </div>
+                <div class="text-content">
+                  Even with the challenges of going back to school, Peter and Anthony understand their need for one another, and that together, they can tackle whatever comes their way.
+                </div>
+              </div>
+            </div>
+            <div class="blank-spacing"></div>
+
+            <div class="timeline-node">
+              <div class="avatar-box">
+                <div>
+                  <a>
+                    <img class="avatar-miniature" src="https://i.imgur.com/UctWXrz.png" />
+                  </a>
+                </div>
+              </div>
+              <div class="content-box">
+                <div class="pre-textual">
+                  <span>Rusro</span>
+                  <span class="action">publicou uma nova resenha</span>
+                </div>
+                <div class="text-content">
+                  Even with the challenges of going back to school, Peter and Anthony understand their need for one another, and that together, they can tackle whatever comes their way.
+                </div>
+              </div>
+            </div>
+            <div class="blank-spacing"></div>
+
+            <div class="timeline-node">
+              <div class="avatar-box">
+                <div>
+                  <a>
+                    <img class="avatar-miniature" src="https://i.imgur.com/UctWXrz.png" />
+                  </a>
+                </div>
+              </div>
+              <div class="content-box">
+                <div class="pre-textual">
+                  <span>Rusro</span>
+                  <span class="action">publicou uma nova resenha</span>
+                </div>
+                <div class="text-content">
+                  Even with the challenges of going back to school, Peter and Anthony understand their need for one another, and that together, they can tackle whatever comes their way.
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
