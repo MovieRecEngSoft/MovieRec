d6625ea3ad281ddfad0832af0e56d76d3cc01f0e
Igor Henrique <igorhenri5@hotmail.com>
2020-09-13 04:15:36 -0300

    Profile Editor Init

---
+++ web/src/Pages/Profile/edit.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+import './styles.css';
+import Menu from "../../Components/Menu";
+import Button from "../../Components/Button";
+
+// Gerenciar perfil
+// Como usuário, eu quero ter um ambiente no sistema que represente meu perfil, onde possa apresentar uma imagem como avatar e compartilhar informações sobre mim.
+
+function ProfileEditor() {
+  return (
+    <>
+      <Menu /> 
+      <div class="wrapper">
+        
+        <div class="profile-block">
+
+        <div class="pfsection txtsection">
+            <div class="txtblk1">
+              <span class="grayed">EDIT YOUR PROFILE</span>
+            </div>
+            <div class="txtblk2" contentEditable="true">
+              <span class="grayed">
+              PREVIEW
+              </span>
+            </div>
+          </div>
+          <div class="blank-spacing"></div>
+          <div class="pfsection imgsection">
+            <div class="pfimgblock">
+              <img class="avatar" src="https://pbs.twimg.com/profile_images/1295555226720641026/gTnCg1RF_400x400.jpg" />
+            </div>
+            <div class="bkground">
+            </div>
+            {/* <div class="bkground">
+            </div> */}
+            <div class="follow-button" >Follow</div>
+            
+            {/* <Button type="button" htmlType="submit" name="Register" class="follow-button" /> */}
+
+          </div>
+          <div class="blank-spacing"></div>
+          <div class="pfsection txtsection">
+            <div class="txtblk1">
+              <span>Rusro</span>
+            </div>
+            <div class="txtblk2" contentEditable="true">
+              <span>
+              #Android is (not) made for everyone. Follow along for the latest updates and stories behind our tech. Questions? Get assistance by using #AndroidHelp.
+              </span>
+            </div>
+          </div>
+          <div class="blank-spacing"></div>
+          <div class="pfsection timeline">
+            <div class="timeline-node">
+              <div class="avatar-box">
+                <div>
+                  <a>
+                    <img class="avatar-miniature" src="https://i.imgur.com/UctWXrz.png" />
+                  </a>
+                </div>
+              </div>
+              <div class="content-box">
+                <div class="pre-textual">
+                  <span>Rusro</span>
+                  <span class="action">está editando seu perfil</span>
+                </div>
+                <div class="text-content">
+                  Esta é uma notificação de teste.
+                </div>
+              </div>
+            </div>
+            {/* <div class="blank-spacing"></div> */}
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+  
+}
+
+export default ProfileEditor;
--- web/src/routes.js
+++ web/src/routes.js
@@ -5,6 +5,7 @@ import FilmDetails from './Pages/FilmDetails';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Profile from './Pages/Profile';
+import ProfileEditor from './Pages/Profile/edit.js';
 import Register from './Pages/Register';
 import Review from './Pages/Review';
 import Search from './Pages/Search';
@@ -17,6 +18,7 @@ function Routes() {
             <Route path="/activity" component={Activity} />
             <Route path="/login" component={Login} />
             <Route path="/movie" component={FilmDetails} />
+            <Route path="/profile/edit" component={ProfileEditor} />
             <Route path="/profile" component={Profile} />
             <Route path="/review" component={Review} />
             <Route path="/signup" component={Register} />
