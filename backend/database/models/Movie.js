b275ccc3423df58b753c15641f2f3d10a2aae7b2
AlaoPrado <alanpradoaraujo@gmail.com>
2020-09-17 10:06:02 -0300

    Add models: Movie, Genre, Company

--- backend/database/models/Movie.js
+++ backend/database/models/Movie.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 const Movie = new mongoose.Schema({
-    title: String
+    title: String,
+    poster_path: String,
+    release_date: Date,
+    overview: String,
+    genres: [{ type: Schema.Types.ObjectId, ref: 'Genre'}],
+    companies: [{ type: Schema.Types.ObjectId, ref: 'Company'}]
 })
 
 module.exports = mongoose.model('Movie', Movie)
