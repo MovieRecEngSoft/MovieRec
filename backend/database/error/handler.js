4fe7ce2df48ce3f46728d96462b908e6de5d28df
AlaoPrado <alanpradoaraujo@gmail.com>
2020-09-18 16:49:45 -0300

    Fix log

--- backend/database/error/handler.js
+++ backend/database/error/handler.js
@@ -2,11 +2,8 @@ const errors = require('./errors.js')
 
 module.exports = {
     handle(error){
-        console.log(error.name)
-        console.log(error.code)
-        if(error.name === 'MongoError' && error.code === 11000){
+        if(error.name === 'MongoError' && error.code === 11000)
             throw new errors.DBDuplicatedKeyError(error.message)
-        }
         else
             throw error
     }
