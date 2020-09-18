c6e1a20d660cfb555ed28c7242733fcf114f5e10
AlaoPrado <alanpradoaraujo@gmail.com>
2020-09-18 16:47:46 -0300

    Fix log

--- backend/database/error/handler.js
+++ backend/database/error/handler.js
@@ -5,7 +5,6 @@ module.exports = {
         console.log(error.name)
         console.log(error.code)
         if(error.name === 'MongoError' && error.code === 11000){
-            console.log("OPA")
             throw new errors.DBDuplicatedKeyError(error.message)
         }
         else
