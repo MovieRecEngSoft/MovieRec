366b2e5465d385875e1970675bbf1b72dd22651c
AlaoPrado <alanpradoaraujo@gmail.com>
2020-09-19 10:29:22 -0300

    Add Movie fields: id_imdb, recommended_movies + Mv metadata

--- backend/database/models/Movie.js
+++ backend/database/models/Movie.js
@@ -5,7 +5,9 @@ const Movie = new mongoose.Schema({
     poster_path: String,
     release_date: Date,
     overview: String,
-    genres: [{ type: Number, ref: 'Genre'}]
+    genres: [{ type: Number, ref: 'Genre'}],
+    id_tmdb: {type: Number, unique: true},
+    recommended_movies: [{type: mongoose.Schema.Types.ObjectId , ref: 'Movie'}]
 })
 
 module.exports = mongoose.model('Movie', Movie)
