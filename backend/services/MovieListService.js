84240fb05d77794fe0df09952ef76c3ef4842ea2
AlaoPrado <alanpradoaraujo@gmail.com>
2020-09-20 19:00:52 -0300

    ADD get /moviesLists + /movieList

--- backend/services/MovieListService.js
+++ backend/services/MovieListService.js
@@ -1,5 +1,9 @@
 const MovieList = require('../database/models/MovieList.js')
 const Movie = require('../database/models/Movie.js')
+const MovieService = require('../services/MovieService.js')
+const MovieFilter = MovieService.MovieFilter
+const SearchParams = MovieService.SearchParams
+
 const assert = require('assert')
 
 module.exports = {
@@ -33,14 +37,32 @@ module.exports = {
         await movieList.save()
     },
 
-    async getMovieList(sesionUserId, name) {
+    async getMovieLists(sesionUserId) {
+        let filter = {
+            user: sesionUserId
+        }
+        const movieLists = await MovieList.find(filter)
+        return movieLists
+    },
+
+    async getMovieListMovies(sesionUserId, name, pageFilter) {
         assert(typeof name === 'string', 'Wrong type of parameter "name".')
         let filter = {
             user: sesionUserId,
             name: name
         }
-        const movieList = await MovieList.find(filter)
-        return movieList
+        const movieList = await MovieList.findOne(filter)
+        assert(movieList, 'Movie list ' + name + ' does not exist.')
+        let movies = []
+        if(movieList.movies.length > 0){
+            let movieFilter = new MovieFilter(
+                undefined, undefined, undefined, undefined, undefined,
+                undefined, movieList.movies
+            )
+            let searchParams = new SearchParams(movieFilter, pageFilter)
+            movies = await MovieService.getMovies(searchParams)
+        }
+        return movies;
     }
 
 }
