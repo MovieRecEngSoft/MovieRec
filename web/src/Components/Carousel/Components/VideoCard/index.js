78cf653effbd71aa5c913891fd1a5112121bdf7b
LorenaMendes <lorenamendes.contato@gmail.com>
2020-09-20 14:15:59 -0300

    added default image for no image movies

--- web/src/Components/Carousel/Components/VideoCard/index.js
+++ web/src/Components/Carousel/Components/VideoCard/index.js
@@ -3,8 +3,19 @@ import { VideoCardContainer } from './styles';
 import { Link } from 'react-router-dom';
 import getImageAddress from '../../../../assets/utils/getImageAddress';
 
+function imageExists(image_url) {
+  var http = new XMLHttpRequest();
+
+  http.open("HEAD", image_url, false);
+  http.send();
+
+  return http.status != 404;
+}
+
 function VideoCard({ videoTitle, videoURL, categoryColor, videoImage }) {
-  const image = getImageAddress(videoImage);
+  const image = imageExists(getImageAddress(videoImage))
+    ? getImageAddress(videoImage)
+    : "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3851270.jpg";
 
   return (
     <Link to="/movie">
