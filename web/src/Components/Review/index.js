8b848b785c8889b6cdfa43e797a021ed7fcdbc64
LorenaMendes <lorenamendes.contato@gmail.com>
2020-09-20 19:50:45 -0300

    added delete review function

--- web/src/Components/Review/index.js
+++ web/src/Components/Review/index.js
@@ -1,37 +1,78 @@
 import React from 'react';
 
-import { LikeOutlined, FullscreenOutlined } from "@ant-design/icons";
+import { LikeOutlined, DeleteOutlined } from "@ant-design/icons";
 import './styles.css';
 
 function Review(props) {
   let reviewClassName = 'review-text';
+  let likesClassName = 'likes';
+  
   if(props.type == "review-item"){
     reviewClassName += ' review-item';
+    likesClassName += ' right-like';
+  }
+
+  function deleteReview(reviewId){
+    fetch("http://localhost:3333/review", {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        reviewId: reviewId,
+      }),
+    });
+    window.location.reload(false);
+  }
+  
+  function like(reviewId){
+    fetch("http://localhost:3333/review/like", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        reviewId: reviewId,
+      }),
+    });
+    window.location.reload(false);
   }
 
   return (
     <>
-    <div className="row">
-      
-      <div className="col">
-        <img className="avatar" src={props.avatar} />
-      </div>
-      <div className="col">
-        <h5>
-          <div className="row space">
+      <div className="row">
+        <div className="col">
+          <img className="avatar" src={props.avatar} />
+        </div>
+        <div className="col">
+          <div className="row">
             <div>
-              <span>Review by </span>{" "}
-              <strong className="reviewer">{props.author}</strong>
+              <h5>
+                <span>Review by </span>{" "}
+                <strong className="reviewer">{props.author}</strong>
+              </h5>
             </div>
-            <div className="likes">
+            <div className={likesClassName}>
+              {props.userId == props.activeUser && (
+                <DeleteOutlined
+                  onClick={() => deleteReview(props.reviewId)}
+                  className="delete-button"
+                />
+              )}
               <p>{props.likes}</p>
-              <LikeOutlined className="like-button" />
+              <LikeOutlined
+                className="like-button"
+                onClick={() => like(props.reviewId)}
+              />
             </div>
           </div>
-        </h5>
-        <p className={reviewClassName}>{props.text}</p>
+          <p className={reviewClassName}>{props.text}</p>
+        </div>
       </div>
-    </div>
     </>
   );
 }
