22e3c401dbf3e35e5be89f4b016b2ce9fe43cd75
Igor Henrique <igorhenri5@hotmail.com>
2020-09-20 01:28:05 -0300

    handleLogin tweak

--- web/src/Components/LoginForm/index.js
+++ web/src/Components/LoginForm/index.js
@@ -8,43 +8,25 @@ import "./styles.css";
 import Input from "../Input";
 import Button from "../Button";
 
-const LoginForm = ({title}) => {
-    const layout = {
-          labelCol: {
-          span: 8,
-        },
-          wrapperCol: {
-          span: 16,
-        },
-    };
+const LoginForm = (props) => {
 
-    const validateMessages = {
-        required: "${label} is required!",
-        types: {
-            email: "${label} is not validate email!",
-            number: "${label} is not a validate number!",
-        },
-        number: {
-            range: "${label} must be between ${min} and ${max}",
-        },
-    };
-
-    const [email, setEmail] = useState("nm");
+  const [email, setEmail] = useState("nm");
     const [password, setPassword] = useState("pss");
 
-    const onFinish = (values) => {
-        console.log(values);
-    };
+    // const onFinish = (values) => {
+    //     console.log(values);
+    // };
 
-    const shoot = () => {
+    const HandleLogin = () => {
       let API_URL = 'http://localhost:3333/login';
       axios.post(API_URL, {
         username: email,
         password: password
       })
       .then(response => {
-        if (response.status == 302) {
-          this.props.history.push('/');
+        if (response.status == 200) {
+          //NICE
+          // this.props.history.push('/');
         } else {
           const error = new Error(response.error);
           throw error;
@@ -57,16 +39,36 @@ const LoginForm = ({title}) => {
 
     }
 
+    const layout = {
+      labelCol: {
+        span: 8,
+      },
+        wrapperCol: {
+        span: 16,
+      },
+    };
+
+    const validateMessages = {
+      required: "${label} is required!",
+        types: {
+            email: "${label} is not validate email!",
+            number: "${label} is not a validate number!",
+        },
+        number: {
+            range: "${label} must be between ${min} and ${max}",
+        },
+    };
+
     return (
       <>
         <div className="content">
           <Form
             {...layout}
             name="nest-messages"
-            onFinish={onFinish}
+            // onFinish={onFinish}
             validateMessages={validateMessages}
           >
-            <h1 className="form-title">{title}</h1>
+            <h1 className="form-title">{props.title}</h1>
 
             <Input name="email" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
 
@@ -77,7 +79,7 @@ const LoginForm = ({title}) => {
             <button
               className="button"
               type="button"
-              onClick={shoot}
+              onClick={HandleLogin}
               htmlType="submit"
               name="Login"
             >Login</button>
