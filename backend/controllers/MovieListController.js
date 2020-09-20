de0c81559953b09f73848f81954d14c4e453d1f9
AlaoPrado <alanpradoaraujo@gmail.com>
2020-09-20 18:38:38 -0300

    Add PUT /movieList + PUT /movieList/movie

--- backend/controllers/MovieListController.js
+++ backend/controllers/MovieListController.js
@@ -1,15 +1,50 @@
-const { getMovieLists } = require('../services/MovieListService.js')
 const MovieListService = require('../services/MovieListService.js')
+const assert = require('assert')
 
 module.exports = {
 
-    async getMovieLists(request, response) {
+    async addMovieList(request, response) {
         try{
             assert(request.isAuthenticated(), 'User must be authenticated to execute this operation.')
-            assert(request.query.name, 'Missing parameter "name"')
+            assert(request.body.name, 'Missing parameter "name".')
             const userId = request.user._id
-            const movieLists = await MovieListService.getMovieLists(userId, request.query.name)
-            response.json(movieLists)
+            await MovieListService.addMovieList(userId, request.body.name)
+            return response.sendStatus(204)
+        }
+        catch(error) {
+            if (error instanceof assert.AssertionError)
+                response.status(400).send(error.toString())
+            else {
+                response.status(500).send(error.toString())
+            }
+        }
+    },
+
+    async addMovieToMovieList(request, response) {
+        try{
+            assert(request.isAuthenticated(), 'User must be authenticated to execute this operation.')
+            assert(request.body.name, 'Missing parameter "name".')
+            assert(request.body.movieId, 'Missing parameter "movieId".')
+            const userId = request.user._id
+            await MovieListService.addMovieToMovieList(userId, request.body.name, request.body.movieId)
+            return response.sendStatus(204)
+        }
+        catch(error) {
+            if (error instanceof assert.AssertionError)
+                response.status(400).send(error.toString())
+            else {
+                response.status(500).send(error.toString())
+            }
+        }
+    },
+
+    async getMovieList(request, response) {
+        try{
+            assert(request.isAuthenticated(), 'User must be authenticated to execute this operation.')
+            assert(request.query.name, 'Missing parameter "name".')
+            const userId = request.user._id
+            const movieList = await MovieListService.getMovieList(userId, request.query.name)
+            response.json(movieList)
         }
         catch(error) {
             if (error instanceof assert.AssertionError)
