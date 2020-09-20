a6c3d1145fdfd90129e3070ef1374866bf8ab794
LorenaMendes <lorenamendes.contato@gmail.com>
2020-09-20 15:51:36 -0300

    url parametrized

--- web/src/Components/Carousel/Components/VideoCard/index.js
+++ web/src/Components/Carousel/Components/VideoCard/index.js
@@ -2,23 +2,17 @@ import React from 'react';
 import { VideoCardContainer } from './styles';
 import { Link } from 'react-router-dom';
 import getImageAddress from '../../../../assets/utils/getImageAddress';
+import checkIfUrlExists from "../../../../assets/utils/checkIfUrlExists";
 
-function imageExists(image_url) {
-  var http = new XMLHttpRequest();
-
-  http.open("HEAD", image_url, false);
-  http.send();
-
-  return http.status != 404;
-}
-
-function VideoCard({ videoTitle, videoURL, categoryColor, videoImage }) {
-  const image = imageExists(getImageAddress(videoImage))
+function VideoCard({ videoTitle, videoId, categoryColor, videoImage }) {
+  const image = checkIfUrlExists(getImageAddress(videoImage))
     ? getImageAddress(videoImage)
     : "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3851270.jpg";
 
+  let path = "/movie/" + videoId;
+  
   return (
-    <Link to="/movie">
+    <Link to={path}>
       <VideoCardContainer
         url={image}
         target="_blank"
