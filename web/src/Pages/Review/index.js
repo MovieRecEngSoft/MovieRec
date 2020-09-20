a6449ca28af2691f25ffd95b8ac9ddda72c1fbad
LorenaMendes <lorenamendes.contato@gmail.com>
2020-09-20 17:24:05 -0300

    added reviews

--- web/src/Pages/Review/index.js
+++ web/src/Pages/Review/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import { LikeOutlined } from "@ant-design/icons";
+import { Link, useParams } from "react-router-dom";
 
 import Menu from '../../Components/Menu';
-import Main from '../../Components/Main';
 import Card from '../../Components/Card';
 import Input from '../../Components/Input';
-import Button from '../../Components/Button';
 import ReviewItem from '../../Components/Review';
 import Comment from '../../Components/Comment';
 import getImageAddress from "../../assets/utils/getImageAddress";
@@ -14,109 +13,123 @@ import getImageAddress from "../../assets/utils/getImageAddress";
 import './styles.css';
 
 function Review(){
-    const image = getImageAddress("/HORpg5CSkmeQlAolx3bKMrKgfi.jpg");
+  let { id } = useParams();
 
-    function shoot() {
-      alert("Comment!");
-      console.log("Comment!");
-    }
+  function addComment(reviewId) {
+    fetch(`http://localhost:3333/review/comment`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        reviewId: reviewId,
+        text: input,
+      }),
+    });
+    window.location.reload(false);
+  }
 
-    return (
-      <>
-        <Menu />
-          <div className="wrapper">
-            <div className="info-block">
+  const [review, setReview] = useState([]);
+  useEffect(() => {
+    const fetchReview = async () => {
+      try {
+        let reviewAux = {};
+        let API_URL = `http://localhost:3333`;
+
+        const result = await axios.get(`${API_URL}/review/?reviewId=${id}`);
+        reviewAux = result.data;
+
+        setReview(reviewAux);
+      } catch (error) {}
+    };
+
+    fetchReview();
+  }, []);
+
+  if (!review.comments) review.comments = [];
+  
+  const image = getImageAddress("/HORpg5CSkmeQlAolx3bKMrKgfi.jpg");
+
+
+
+  const [input, setInput] = useState("");
+
+  return (
+    <>
+      <Menu />
+      <div className="wrapper">
+        <div className="info-block">
+          <div className="row">
+            <div className="review-leftcolumn">
               <div className="row">
-                <div className="review-leftcolumn">
-                  <div className="row">
-                    <Card>
-                      <h2 className="film-title">
-                        <strong>Skyfall - 007</strong>
-                      </h2>
-                      <br />
-                      <ReviewItem
-                        className="review-text"
-                        text="Sunt in culpa qui officia deserunt mollit anim id est laborum
-                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco. Sunt in culpa qui officia deserunt mollit anim id est laborum
-                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco.
-                        Sunt in culpa qui officia deserunt mollit anim id est laborum
-                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco.Sunt in culpa qui officia deserunt mollit anim id est laborum
-                        consectetur adipiscing elit."
-                        author="Lorena Mendes"
-                        avatar="https://trello-members.s3.amazonaws.com/5ca54b957891488995401310/cd44fb76a31dbd7f303e3feac211c9e3/50.png"
-                      />
-                      <br />
-                    </Card>
-                  </div>
+                <Card>
+                  <h2 className="film-title">
+                    <strong>Skyfall - 007</strong>
+                  </h2>
                   <br />
-                  <h1 className="title-separator">Comments</h1>
-                  <Card>
-                    <div className="comment-session">
-                      <Input className="add-comment-input" />
-                      <button
-                        className="add-comment-button"
-                        name="Add comment"
-                        onClick={shoot}
-                      >Add comment
-                      </button>
-                    </div>
-                  </Card>
-                  <Card>
-                    <Comment
-                      text="Sunt in culpa qui officia deserunt mollit anim id est laborum
-                          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                          exercitation ullamco."
-                      author="Marco Tulio"
-                      avatar="https://avatars3.githubusercontent.com/u/213200?s=460&u=c2b5074042aa87fb9561108f95dfc04abcd646b1&v=4"
-                    />
-                  </Card>
-                  <Card>
-                    <Comment
-                      text="Sunt in culpa qui officia deserunt mollit anim id est laborum
-                          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                          exercitation ullamco."
-                      author="Marco Tulio"
-                      avatar="https://avatars3.githubusercontent.com/u/213200?s=460&u=c2b5074042aa87fb9561108f95dfc04abcd646b1&v=4"
-                    />
-                  </Card>
-                  <Card>
-                    <Comment
-                      text="Sunt in culpa qui officia deserunt mollit anim id est laborum
-                          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                          exercitation ullamco."
-                      author="Marco Tulio"
-                      avatar="https://avatars3.githubusercontent.com/u/213200?s=460&u=c2b5074042aa87fb9561108f95dfc04abcd646b1&v=4"
-                    />
-                  </Card>
-                  <Card>
-                    <Comment
-                      text="Sunt in culpa qui officia deserunt mollit anim id est laborum
-                          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                          exercitation ullamco."
-                      author="Marco Tulio"
-                      avatar="https://avatars3.githubusercontent.com/u/213200?s=460&u=c2b5074042aa87fb9561108f95dfc04abcd646b1&v=4"
-                    />
-                  </Card>
+
+                  <ReviewItem
+                    className="review-text"
+                    text={review.text}
+                    likes={review.likes}
+                    author={review.username}
+                    avatar={
+                      !review.userImgUrl
+                        ? "https://simpleicon.com/wp-content/uploads/user1.png"
+                        : review.userImgUrl
+                    }
+                  />
                   <br />
-                </div>
-                <div className="review-rightcolumn">
-                    <img className="cover" src={image} />
-                </div>
+                </Card>
               </div>
+              <br />
+              <h1 className="title-separator">Comments</h1>
+              <Card>
+                <div className="comment-session">
+                  <Input
+                    className="add-comment-input"
+                    value={input}
+                    onInput={(e) => setInput(e.target.value)}
+                  />
+                  <button
+                    className="add-comment-button"
+                    name="Add comment"
+                    onClick={() => addComment(id)}
+                  >
+                    Add comment
+                  </button>
+                </div>
+              </Card>
+
+              {review.comments
+                .slice(0)
+                .reverse()
+                .map((comment, index) => {
+                  return (
+                    <Card>
+                      <Comment
+                        text={comment.text}
+                        author={comment.username}
+                        avatar={
+                          !review.userImgUrl
+                            ? "https://simpleicon.com/wp-content/uploads/user1.png"
+                            : review.userImgUrl}
+                      />
+                    </Card>
+                  );
+                })}
+              <br />
+            </div>
+            <div className="review-rightcolumn">
+              <img className="cover" src={image} />
             </div>
           </div>
-      </>
-    );
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Review;
