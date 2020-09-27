aeb6ac6cc5302a7e36d0a480eafa41386a7fbbd7
Igor Henrique <igorhenri5@hotmail.com>
2020-09-27 14:43:17 -0300

    Update testsIgor.js

--- web/src/Tests/testsIgor.js
+++ web/src/Tests/testsIgor.js
@@ -3,7 +3,7 @@ import { Selector } from 'testcafe';
 fixture `Frontend tests by Igor`
     .page `http://localhost:3000`;
 
-/*
+/* 0
 test('Submit all required fields and login', async t => {
     const username = 'rusro';
     const password = 'password';
@@ -31,166 +31,171 @@ test('Submit all required fields and login', async t => {
 });
 */
 
-// test('Logout', async t => {
+//1
+test('Logout', async t => {
 
-//     const username = 'rusro';
-//     const password = 'password';
+    const username = 'rusro';
+    const password = 'password';
 
-//     const userNameInput = Selector('#name');
-//     const userNameInputExists = userNameInput.exists;
+    const userNameInput = Selector('#name');
+    const userNameInputExists = userNameInput.exists;
 
-//     const passwordInput = Selector('#password');
-//     const passwordInputExists = passwordInput.exists;
+    const passwordInput = Selector('#password');
+    const passwordInputExists = passwordInput.exists;
 
-//     const submitButton = Selector('#submit');
-//     const submitButtonExists = submitButton.exists;       
+    const submitButton = Selector('#submit');
+    const submitButtonExists = submitButton.exists;       
 
-//     const logoutButton = Selector('.logout-button');
-//     const logoutButtonExists = logoutButton.exists;
+    const logoutButton = Selector('.logout-button');
+    const logoutButtonExists = logoutButton.exists;
 
-//     await t
-//         .expect(userNameInputExists).ok()
-//         .typeText(userNameInput, username)
-//         .expect(userNameInput.value).eql(username)
+    await t
+        .expect(userNameInputExists).ok()
+        .typeText(userNameInput, username)
+        .expect(userNameInput.value).eql(username)
 
-//         .expect(passwordInputExists).ok()
-//         .typeText(passwordInput, password)
-//         .expect(passwordInput.value).eql(password)
+        .expect(passwordInputExists).ok()
+        .typeText(passwordInput, password)
+        .expect(passwordInput.value).eql(password)
 
-//         .expect(submitButtonExists).ok()
-//         .click(submitButton)
+        .expect(submitButtonExists).ok()
+        .click(submitButton)
 
-//         .expect(logoutButtonExists).ok()
-//         .click(logoutButton)
-// });
+        .expect(logoutButtonExists).ok()
+        .click(logoutButton)
+});
 
-// test('Edit and save profile description', async t => {
+//2
+test('Edit and save profile description', async t => {
 
-//     const username = 'rusro';
-//     const password = 'password';
+    const username = 'rusro';
+    const password = 'password';
 
-//     const userNameInput = Selector('#name');
-//     const userNameInputExists = userNameInput.exists;
+    const userNameInput = Selector('#name');
+    const userNameInputExists = userNameInput.exists;
 
-//     const passwordInput = Selector('#password');
-//     const passwordInputExists = passwordInput.exists;
+    const passwordInput = Selector('#password');
+    const passwordInputExists = passwordInput.exists;
 
-//     const submitButton = Selector('#submit');
-//     const submitButtonExists = submitButton.exists;       
+    const submitButton = Selector('#submit');
+    const submitButtonExists = submitButton.exists;       
 
-//     const selfAvatar = Selector('.avatar');
-//     const selfAvatarExists = selfAvatar.exists;
+    const selfAvatar = Selector('.avatar');
+    const selfAvatarExists = selfAvatar.exists;
 
-//     const editButton = Selector('#edit-button');
-//     const editButtonExists = editButton.exists;
+    const editButton = Selector('#edit-button');
+    const editButtonExists = editButton.exists;
 
-//     const descriptionInput = Selector('#description-input');
-//     const descriptionInputExists = descriptionInput.exists;
+    const descriptionInput = Selector('#description-input');
+    const descriptionInputExists = descriptionInput.exists;
 
-//     const applyChangesButton = Selector('#apply');
-//     const applyChangesButtonExists = applyChangesButton.exists;
+    const applyChangesButton = Selector('#apply');
+    const applyChangesButtonExists = applyChangesButton.exists;
 
-//     await t
-//         .expect(userNameInputExists).ok()
-//         .typeText(userNameInput, username)
-//         .expect(userNameInput.value).eql(username)
+    await t
+        .expect(userNameInputExists).ok()
+        .typeText(userNameInput, username)
+        .expect(userNameInput.value).eql(username)
 
-//         .expect(passwordInputExists).ok()
-//         .typeText(passwordInput, password)
-//         .expect(passwordInput.value).eql(password)
+        .expect(passwordInputExists).ok()
+        .typeText(passwordInput, password)
+        .expect(passwordInput.value).eql(password)
 
-//         .expect(submitButtonExists).ok()
-//         .click(submitButton)
+        .expect(submitButtonExists).ok()
+        .click(submitButton)
 
-//         .expect(selfAvatarExists).ok()
-//         .click(selfAvatar)
+        .expect(selfAvatarExists).ok()
+        .click(selfAvatar)
 
-//         .expect(editButtonExists).ok()
-//         .click(editButton)
+        .expect(editButtonExists).ok()
+        .click(editButton)
 
-//         .expect(descriptionInputExists).ok()
-//         .click(descriptionInput)
-//         .pressKey('ctrl+a delete')
-//         .typeText(descriptionInput, "Hello! I'm testing features using TestCafe.")
+        .expect(descriptionInputExists).ok()
+        .click(descriptionInput)
+        .pressKey('ctrl+a delete')
+        .typeText(descriptionInput, "Hello! I'm testing features using TestCafe.")
 
-//         .expect(applyChangesButtonExists).ok()
-//         .click(applyChangesButton)
-// });
+        .expect(applyChangesButtonExists).ok()
+        .click(applyChangesButton)
+});
 
-// test('Follow/Unfollow another user', async t => {
+//3
+test('Follow/Unfollow another user', async t => {
 
-//     const username = 'rusro';
-//     const password = 'password';
+    const username = 'rusro';
+    const password = 'password';
 
-//     const userNameInput = Selector('#name');
-//     const userNameInputExists = userNameInput.exists;
+    const userNameInput = Selector('#name');
+    const userNameInputExists = userNameInput.exists;
 
-//     const passwordInput = Selector('#password');
-//     const passwordInputExists = passwordInput.exists;
+    const passwordInput = Selector('#password');
+    const passwordInputExists = passwordInput.exists;
 
-//     const submitButton = Selector('#submit');
-//     const submitButtonExists = submitButton.exists;       
+    const submitButton = Selector('#submit');
+    const submitButtonExists = submitButton.exists;       
 
 
-//     const userAvatar = Selector('.avatar-miniature');
-//     const userAvatarExists = userAvatar.exists;
+    const userAvatar = Selector('.avatar-miniature');
+    const userAvatarExists = userAvatar.exists;
 
-//     const followButton = Selector('#follow-button');
-//     const followButtonExists = followButton.exists;
+    const followButton = Selector('#follow-button');
+    const followButtonExists = followButton.exists;
 
-//     await t
-//         .expect(userNameInputExists).ok()
-//         .typeText(userNameInput, username)
-//         .expect(userNameInput.value).eql(username)
+    await t
+        .expect(userNameInputExists).ok()
+        .typeText(userNameInput, username)
+        .expect(userNameInput.value).eql(username)
 
-//         .expect(passwordInputExists).ok()
-//         .typeText(passwordInput, password)
-//         .expect(passwordInput.value).eql(password)
+        .expect(passwordInputExists).ok()
+        .typeText(passwordInput, password)
+        .expect(passwordInput.value).eql(password)
     
-//         .expect(submitButtonExists).ok()
-//         .click(submitButton)
+        .expect(submitButtonExists).ok()
+        .click(submitButton)
 
-//         .expect(userAvatarExists).ok()
-//         .click(userAvatar)
+        .expect(userAvatarExists).ok()
+        .click(userAvatar)
 
-//         .expect(followButtonExists).ok()
-//         // .click(followButton)
+        .expect(followButtonExists).ok()
+        // .click(followButton)
 
-// });
+});
 
-// test('Visit an activity of an user you follow', async t => {
+//4
+test('Visit an activity of an user you follow', async t => {
 
-//     const username = 'rusro';
-//     const password = 'password';
+    const username = 'rusro';
+    const password = 'password';
 
-//     const userNameInput = Selector('#name');
-//     const userNameInputExists = userNameInput.exists;
+    const userNameInput = Selector('#name');
+    const userNameInputExists = userNameInput.exists;
 
-//     const passwordInput = Selector('#password');
-//     const passwordInputExists = passwordInput.exists;
+    const passwordInput = Selector('#password');
+    const passwordInputExists = passwordInput.exists;
 
-//     const submitButton = Selector('#submit');
-//     const submitButtonExists = submitButton.exists;       
+    const submitButton = Selector('#submit');
+    const submitButtonExists = submitButton.exists;       
 
-//     const ActivityLink = Selector('.action a');
-//     const ActivityLinkExists = ActivityLink.exists;
+    const ActivityLink = Selector('.action a');
+    const ActivityLinkExists = ActivityLink.exists;
 
-//     await t
-//         .expect(userNameInputExists).ok()
-//         .typeText(userNameInput, username)
-//         .expect(userNameInput.value).eql(username)
+    await t
+        .expect(userNameInputExists).ok()
+        .typeText(userNameInput, username)
+        .expect(userNameInput.value).eql(username)
 
-//         .expect(passwordInputExists).ok()
-//         .typeText(passwordInput, password)
-//         .expect(passwordInput.value).eql(password)
+        .expect(passwordInputExists).ok()
+        .typeText(passwordInput, password)
+        .expect(passwordInput.value).eql(password)
 
-//         .expect(submitButtonExists).ok()
-//         .click(submitButton)
+        .expect(submitButtonExists).ok()
+        .click(submitButton)
 
-//         .expect(ActivityLinkExists).ok()
-//         .click(ActivityLink)
-// });
+        .expect(ActivityLinkExists).ok()
+        .click(ActivityLink)
+});
 
+//5
 test('Search for a movie by name, using the top searchbar', async t => {
     const username = 'rusro';
     const password = 'password';
