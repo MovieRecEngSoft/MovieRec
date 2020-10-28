dbcc39dd4543c8e3d56b36764b70301e1ebeec2d
AlaoPrado <alanpradoaraujo@gmail.com>
2020-10-28 20:20:15 -0300

    Add mock test + config jest env

--- backend/tests/services/MovieService.test.js
+++ backend/tests/services/MovieService.test.js
@@ -3,8 +3,14 @@ const MovieFilter = MovieService.MovieFilter
 const PageFilter = MovieService.PageFilter
 const assert = require('assert')
 const mongoose = require('mongoose')
+const mockingoose = require('mockingoose').default
+const Movie = require('../../database/models/Movie')
 
-test('MovieFilter: validate', () => {
+afterEach(() => {
+    mockingoose.resetAll()
+})
+
+test('MovieService: MovieFilter: validate', () => {
     //Default parameters
     let movieFilter = new MovieFilter()
     expect(() => movieFilter.validate()).not.toThrow()
@@ -46,7 +52,7 @@ test('MovieFilter: validate', () => {
 })
 
 
-test('PageFilter: validate', () => {
+test('MovieService: PageFilter: validate', () => {
     //Default parameters
     let pageFilter = new PageFilter()
     expect(() => pageFilter.validate()).not.toThrow()
@@ -70,7 +76,7 @@ test('PageFilter: validate', () => {
     expect(() => pageFilter.validate()).toThrow(assert.AssertionError)
 })
 
-test('PageFilter: size', () => {
+test('MovieService: PageFilter: size', () => {
     let pageFilter = new PageFilter(1,10)
     expect(pageFilter.skip).toBe(0)
     pageFilter = new PageFilter(2,10)
@@ -79,4 +85,55 @@ test('PageFilter: size', () => {
     expect(pageFilter.skip).toBe(20)
     pageFilter = new PageFilter(3,20)
     expect(pageFilter.skip).toBe(40)
+})
+
+test('MovieService: size', async () => {
+    const movieId = mongoose.Types.ObjectId()
+    const title = 'Toy Story'
+    const overview = 'Good'
+    const genres = [12, 16]
+    const poster_path = '/axzklueuxhkfnzlxYsSXJ'
+    const release_date = '2010-06-16T00:00:00.000Z'
+    const recommended_movies = [
+        "5f66e4585d908d3aa007b7a6",
+        "5f66e4595d908d3aa0080f62",
+        "5f66e45a5d908d3aa0082323",
+        "5f66e45a5d908d3aa0082329",
+        "5f66e45a5d908d3aa0081e8e",
+        "5f66e4595d908d3aa007d6ba",
+        "5f66e4595d908d3aa007d7f4",
+        "5f66e45a5d908d3aa00824bf",
+        "5f66e45a5d908d3aa008529b"
+    ]
+    const _doc = {
+        _id: movieId,
+        title: title,
+        poster_path: poster_path,
+        release_date: release_date,
+        overview: overview,
+        genres: genres,
+        recommended_movies: recommended_movies
+    }
+    const finderMock = query => {
+        if (query.getQuery()._id === movieId) {
+            return _doc;
+        }
+        else{
+            return undefined
+        }
+    }
+    mockingoose(Movie).toReturn(finderMock, 'findOne')
+    //Movie found
+    let movie = await MovieService.getMovie(movieId)
+    expect(movie._id).toBe(movie._id)
+    expect(movie.title).toBe(movie.title)
+    expect(movie.poster_path).toBe(movie.poster_path)
+    expect(movie.release_date).toBe(movie.release_date)
+    expect(movie.overview).toBe(movie.overview)
+    expect(movie.genres).toBe(movie.genres)
+    expect(movie.recommended_movies).toBe(movie.recommended_movies)
+    //Movie not found
+    movie = await MovieService.getMovie(mongoose.Types.ObjectId())
+    expect(movie).toBe(undefined)
+
 })
