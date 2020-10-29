2b9c73613ee3673dee17ea3859972bd4a70567dc
AlaoPrado <alanpradoaraujo@gmail.com>
2020-10-28 21:54:04 -0300

    Rm console.log

--- backend/tests/services/Search.test.js
+++ backend/tests/services/Search.test.js
@@ -3,12 +3,13 @@ const connection = require('../../database/connection')
 
 jest.unmock('../../database/models/Movie')
 
-test('MovieService: search', () => {
+test('MovieService: search stages', () => {
     connection.connect().then(async _ =>{
+        //searching for a page
         let movies = await MovieService.getMovies()
+        //searching for a movie in the retrieved page
         let movie = await MovieService.getMovie(movies[0]._id)
         expect(movie._id).toBe(movies[0]._id)
-        console.log(movies)
         connection.close()
     })
 })
